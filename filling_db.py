d0909ebc9eb936f3798a010b8db69767b842aae1
Dmitry <mazaxist2@mail.ru>
2019-04-06 13:27:27 +0300

    added folder "statis" and "media", added ways to images in base.html

--- filling_db.py
+++ filling_db.py
@@ -1,6 +1,6 @@
 import psycopg2
 import csv
-from webapp.db import db
+from webapp.db import db, Author
 from webapp.book.models import Book
 from webapp import create_app
 
@@ -12,10 +12,12 @@ app.app_context().push()
 with open('books.csv', 'r', encoding='utf-8') as f:
 	books = csv.DictReader(f, delimiter=';')
 	for row in books:
-		book_data = Book(name=row['name'], genre=row['category'], description=row['description'], price=row['price'])
-		db.session.add(book_data)
 		author_data = Author(first_name=row['first_name'], last_name=row['last_name'], birth_date=row['birth_date'], description=row['author_description'])
 		db.session.add(author_data)
+		db.session.flush()		
+		print(author_data.id)
+		book_data = Book(name=row['name'], genre=row['category'], description=row['description'], price=row['price'], image=row['image'], author_id=author_data.id)
+		db.session.add(book_data)
 
 db.session.commit()
 conn.close()
