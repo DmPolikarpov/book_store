95cd42d9a0b4b8595e872dc0d2744c249cf1d936
Karolina <gorska.karoliewa@gmail.com>
2019-04-14 10:24:24 +0300

    models added, db corrected, first migration, authors page added

--- webapp/author/views.py
+++ webapp/author/views.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template
+from webapp.author.models import Author
 
 blueprint = Blueprint('author', __name__, url_prefix='/author')
 
 @blueprint.route('/')
 def author_index():
 	title = 'Авторы'
-	return render_template('author/index.html', page_title=title)
+	author_list = Author.query.order_by(Author.id.desc()).all()
+	return render_template('author/index.html', page_title=title, author_list=author_list)
