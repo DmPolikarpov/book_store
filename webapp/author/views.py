20ebbcec03f2a9cff4e2f91b6a42e85b3aff5707
Karolina <gorska.karoliewa@gmail.com>
2019-04-20 11:17:57 +0300

    authors page added

--- webapp/author/views.py
+++ webapp/author/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from webapp.author.models import Author
 
 blueprint = Blueprint('author', __name__, url_prefix='/author')
@@ -8,3 +8,10 @@ def author_index():
 	title = 'Авторы'
 	author_list = Author.query.order_by(Author.id.desc()).all()
 	return render_template('author/index.html', page_title=title, author_list=author_list)
+
+@blueprint.route('author_id', methods=['POST'])
+def author_id():
+	author_id = int(request.form['author_id'])
+	author = Author.query.filter(Author.id == author_id).first()
+	title = "О авторе"
+	return render_template("author/more.html", page_title=title, author=author)
--- webapp/book/views.py
+++ webapp/book/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from webapp.book.models import Book
 
 
@@ -9,3 +9,11 @@ def book_index():
 	title = "Книги"
 	book_list = Book.query.order_by(Book.id.desc()).all()
 	return render_template("book/index.html", page_title=title, book_list=book_list)
+
+
+@blueprint.route('book_id', methods=['POST'])
+def book_id():
+	book_id = int(request.form['book_id'])
+	book = Book.query.filter(Book.id == book_id).first()
+	title = "О книге"
+	return render_template("book/more.html", page_title=title, book=book)
