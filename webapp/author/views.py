57f08d997869cff753e5ef94670203d3550a234c
Karolina <gorska.karoliewa@gmail.com>
2019-04-20 12:45:00 +0300

    author corrected

--- webapp/author/views.py
+++ webapp/author/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, abort
 from webapp.author.models import Author
 
 blueprint = Blueprint('author', __name__, url_prefix='/author')
@@ -9,9 +9,8 @@ def author_index():
 	author_list = Author.query.order_by(Author.id.desc()).all()
 	return render_template('author/index.html', page_title=title, author_list=author_list)
 
-@blueprint.route('author_id', methods=['POST'])
-def author_id():
-	author_id = int(request.form['author_id'])
-	author = Author.query.filter(Author.id == author_id).first()
+@blueprint.route('<int:author_id>', methods=['GET'])
+def author_info(author_id):
+	author = Author.query.get_or_404(author_id)
 	title = "О авторе"
 	return render_template("author/more.html", page_title=title, author=author)
